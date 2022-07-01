fd10e9505762aeca18af704d74ce8120edc915fa
0Thomas1 <99365833+0Thomas1@users.noreply.github.com>
2022-06-30 23:46:39 -0700

    Initial commit

---
+++ Coder.java
@@ -0,0 +1,105 @@
+/*
+File name: Coder.java
+Written by Thomas Cheng
+Student id: 100388047
+Written on 30 June 2022
+
+* input message to encrypt and the key
+* output encrypted message
+*/
+import java.util.Scanner;
+public class Coder {
+    /** Test program to invoke myInput and print the message a character per line */
+    public static void main (String[] args) {
+
+        char[] msg;
+        int key;
+        Scanner input = new Scanner(System.in);
+        msg = getMsg(input); // Read the message from keyboard
+        key = getKey(input);
+        System.out.println("Key : "+key);
+        String out ="";
+        for (char character : msg) {
+            out += ascii2binary(cipher(character, key));
+        }
+        System.out.println(out);
+    }
+    //--------
+    /** Read a line of message from keyboard and return it as an array of char
+     @return : Array of characters
+     */
+    public static char[] getMsg(Scanner input){
+        String myMessage;
+        System.out.print("Input Message: ");
+        myMessage = input.nextLine();// Read a line of message
+        return myMessage.toCharArray();
+    }
+    //--------
+    /** Read an integer from keyboard and return it
+     @return : code
+     */
+    public static int getKey(Scanner input){
+        //int code=0;
+        System.out.print("Input Key: ");
+        return input.nextInt();
+    }
+
+    /**
+     * encrypt char by the key
+     * @param chars: input character
+     * @param key: encryption key
+     * @return encrypted character
+     */
+    public static char cipher(char chars, int key){
+        if ((chars>= '0') && (chars <='9')){
+            chars = cipherPart(chars,key,'0','9');
+        }
+        else if ((chars>= 'A') && (chars <='Z')) {
+            chars = cipherPart(chars,key,'A','Z');
+        }
+        else if ((chars>= 'a') && (chars <='z')){
+            chars = cipherPart(chars,key,'a','z');
+        }
+        return chars;
+    }
+
+    /**
+     * parts of the cipher
+     * @param chars: character to be encrypted
+     * @param key: encryption key
+     * @param low: character with the lowest ascii value
+     * @param up: character with the biggest ascii value
+     * @return encrypted character
+     */
+    public static char cipherPart(char chars, int key, char low, char up){
+        chars +=key;
+        if (chars > up){
+            chars =(char)(chars - up + low-1);
+        }
+        return chars;
+    }
+
+    /**
+     * takes ascii value and translate it to 8bit binary number
+     * @param n: ascii value
+     * @return binary value
+     */
+    public static String ascii2binary(int n) {
+        String binary = "";
+        int digit;
+        int pad = 8;
+        while (n>0){ //translate ascii to binary
+            digit = n%2;
+            binary =digit + binary;
+            n/=2;
+            pad -=1;
+        }
+
+        // pad the number into an 8-bit binary number
+        while (pad >0){
+            binary = "0" + binary;
+            pad -=1;
+        }
+        return binary;
+    }
+}
---
+++ Decoder.java
@@ -0,0 +1,125 @@
+/*
+File name: Decoder.java
+Written by Thomas Cheng
+Student id: 100388047
+Written on 30 June 2022
+
+* input encrypted message and the key
+* output decrypted message
+*/
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Decoder {
+    public static void main(String[] args) {
+        // create scanner object
+        Scanner in = new Scanner(System.in);
+
+        // initialize the key
+        int key = getKey(in);
+
+        // ket confirmation
+        System.out.println("Key : "+key);
+
+        // chop input into manageable 8-bit binary string array
+        List<String> outLst = binStringChoppa(getMsg(in));
+
+        //print decrypted character one by one
+        for (String eightBitNum: outLst) {
+            System.out.print(decipher((char)binary2ascii(eightBitNum),key));
+        }
+    }
+
+    /**
+     * prompt user to enter decryption key
+     * @param input : input object
+     * @return : key
+     */
+    public static int getKey(Scanner input){
+        System.out.print("Input Key: ");
+        return input.nextInt();
+    }
+
+    /**
+     * Read a line of message from keyboard and return it as a string of char
+     * @return String of characters
+     */
+    public static String getMsg(Scanner input) {
+        String myMessage;
+        input.nextLine();
+        System.out.print("Input Message: ");
+        myMessage = input.nextLine();
+        return myMessage;
+    }
+
+    /**
+     * Takes character and key to decrypt the character
+     * @param chars : character to decrypt
+     * @param key : the decryption key
+     * @return : decrypted character
+     */
+    public static char decipher(char chars, int key) {
+        if ((chars >= '0') && (chars <= '9')) {// for character set 0-9
+            chars = decipherPart(chars, key, '0', '9');
+        }
+        else if ((chars >= 'A') && (chars <= 'Z')) {// for character set A-Z
+            chars = decipherPart(chars, key, 'A', 'Z');
+        }
+        else if ((chars >= 'a') && (chars <= 'z')) {// for character set a-z
+            chars = decipherPart(chars, key, 'a', 'z');
+        }
+        return chars;
+    }
+
+    /**
+     * decrypt character from a certain set of characters
+     * @param chars : characters to be decrypted
+     * @param key : decryption key
+     * @param low : lowest value of the character set
+     * @param up : highest value of the character set
+     * @return decrypted character
+     */
+    public static char decipherPart(char chars, int key, char low, char up) {
+        chars -= key;
+        if (chars < low) {// wraps around to the top
+            chars = (char) ( up- low + chars+1);
+        }
+        return chars;
+    }
+
+    /**
+     * takes a 8bit binary number in string and turn it into ascii value
+     * @param bin: 8-bit number
+     * @return ascii value
+     */
+    public static int binary2ascii(String bin) {
+        int num = 0;
+        char[] binary = bin.toCharArray();
+        for (int i = 0; i < binary.length; i++) {// for each digit in the binary number
+            if (binary[i] == '1') {
+                num += Math.pow(2, binary.length- 1 - i);
+            }
+        }
+        return num;
+    }
+
+    /**
+     * Takes a String and chop it into a string array with a 8bit binary number in each cell
+     * @param bin: binary number string
+     * @return String list
+     */
+    public static List<String> binStringChoppa(String bin) {
+        char[] binCharArray = bin.toCharArray();
+        List<String> stringArray = new ArrayList<>();
+        for (int k = 0; k < binCharArray.length / 8; k++) {// for every 8 digit
+            String strToAdd="";
+            for (int i = k * 8; i <= k*8 + 7; i++) {// isolate 8-bit binary number as a string
+                strToAdd += Character.toString(binCharArray[i]);
+            }
+            // insert the 8-bit binary number string into a new cell in the Array
+            stringArray.add(strToAdd);
+        }
+        return stringArray;
+    }
+}
